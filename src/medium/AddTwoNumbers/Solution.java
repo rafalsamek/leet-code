19c84c4848fd8ea3227c1c13a155cb0fd6db458d
Rafal Samek <rsamek@datto.com>
2025-01-27 21:40:56 +0100

    medium.AddTwoNumbers - implemented

--- src/medium/AddTwoNumbers/Solution.java
+++ src/medium/AddTwoNumbers/Solution.java
@@ -46,6 +46,89 @@ package medium.AddTwoNumbers;
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode(0);
+        checkConstraints(l1, l2);
+        int i1 = convertListNodeToInt(l1);
+        int i2 = convertListNodeToInt(l2);
+
+        return convertIntToListNode(i1 + i2);
+    }
+
+    private void checkConstraints(ListNode l1, ListNode l2) {
+        int l1NumberOfNodes = numberOfNodes(l1);
+        int l2NumberOfNodes = numberOfNodes(l2);
+        if (l1NumberOfNodes < 1 || l1NumberOfNodes > 100 || l2NumberOfNodes < 1 || l2NumberOfNodes > 100) {
+            throw new IllegalArgumentException(
+                    "The number of nodes in each linked list should be in the range [1, 100]."
+            );
+        }
+
+        checkNodeVals(l1);
+        checkNodeVals(l2);
+
+        checkLeadingZeros(l1);
+        checkLeadingZeros(l2);
+    }
+
+    private void checkLeadingZeros(ListNode listNode) {
+        StringBuilder intNumber = new StringBuilder();
+        while (listNode != null) {
+            intNumber.append(listNode.val);
+            listNode = listNode.next;
+        }
+
+        String intNumberString1 = intNumber.reverse().toString();
+        String intNumberString2 = String.valueOf(Integer.parseInt(intNumberString1));
+
+        if (intNumberString1.length() != intNumberString2.length()) {
+            throw new IllegalArgumentException(
+                    "It should be guaranteed that the list represents a number that does not have leading zeros."
+            );
+        }
+    }
+
+    private void checkNodeVals(ListNode listNode) {
+        while (listNode != null) {
+            if (listNode.val < 0 || listNode.val > 9) {
+                throw new IllegalArgumentException(
+                        "should be: 0 <= Node.val <= 9"
+                );
+            }
+            listNode = listNode.next;
+        }
+    }
+
+    private int numberOfNodes(ListNode listNode) {
+        int numberOfNodes = 0;
+        while (listNode != null) {
+            numberOfNodes++;
+            listNode = listNode.next;
+        }
+
+        return numberOfNodes;
+    }
+
+    private int convertListNodeToInt(ListNode listNode) {
+        StringBuilder intNumber = new StringBuilder();
+        while (listNode != null) {
+            intNumber.append(listNode.val);
+            listNode = listNode.next;
+        }
+
+        return Integer.parseInt(intNumber.reverse().toString());
+    }
+
+    private ListNode convertIntToListNode(int intNumber) {
+        String intNumberString = new StringBuilder(String.valueOf(intNumber)).reverse().toString();
+        ListNode listNode = new ListNode();
+        ListNode head = listNode;
+        for (int i = 0; i < intNumberString.length(); i++) {
+            listNode.val = Integer.parseInt(String.valueOf(intNumberString.charAt(i)));
+            if (i < intNumberString.length() - 1) {
+                listNode.next = new ListNode();
+                listNode = listNode.next;
+            }
+        }
+
+        return head;
     }
 }
